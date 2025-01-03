8366243eef0c9c77d5d8cdd320e59ce16b72d45f
kaua-cmyk <kaualuizrodriguessilva3@gmail.com>
2025-01-03 18:41:35 -0300

    index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["By Kauã 🚀"];
+const statusMessages = ["ITÁLIA RP 😈 VIP GRÁTIS + CARRO INICIAL 💎 + 200 FPS"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
